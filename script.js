3e9b716308e7d04cec36f506181c9702d7e1d691
unknown <riyas.rio18@gmail.com>
2024-11-04 13:11:53 +0530

    created card component

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('product_add').addEventListener('click', function() {
+    this.disabled = true;
+    alert('Item added to cart!'); 
+});
+
+
+document.getElementById('product_add1').addEventListener('click', function() {
+    this.disabled = true;
+    alert('Item added to cart!'); 
+});
+
+document.getElementById('product_add2').addEventListener('click', function() {
+    this.disabled = true;
+    alert('Item added to cart!'); 
+});
